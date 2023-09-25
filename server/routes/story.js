fc180c59e84046b52bdb128a86a43e77cbc8a038
swing <swing.li@my.cityu.edu.hk>
2023-09-25 20:00:40 +0800

    update

--- server/routes/story.js
+++ server/routes/story.js
@@ -4,7 +4,19 @@ const router = express.Router();
 
 const Story = require('../models/Story');
 
+router.post('/',(req,res,next)=>{
+  console.log("save story");
+  const story = new Story(req.body);
+  const promise = story.save();
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+})
+
 router.get('/count',(req,res)=>{
+  console.log("do this1")
   const promise = Story.aggregate([
     {
       $group:{
@@ -20,7 +32,50 @@ router.get('/count',(req,res)=>{
   })
 })
 
+router.get('/:id',(req,res)=>{
+  const promise = Task.aggregate([
+    {
+      $match:{
+        storyId:  parseInt(req.params.id)
+      }
+    },
+    {
+      $lookup:{
+        from:'users',
+        localField:'contributors',
+        foreignField:'_id',
+        as:'contributors'
+      }
+    },
+    {
+      $unwind:{
+        path:'$contributors'
+      }
+    },
+    {
+      $group:{
+        _id:{
+          _id:'$_id',
+          title:'$title',
+          createdBy:'$createdBy',
+          createdDate:'$createdDate'
+        },
+        contributors:{
+        $push:'$contributors'
+    }
+  }
+  },
+    
+  ]);
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+})
+
 router.post('/',(req,res,next)=>{
+  console.log("do this2")
   const story = new Story(req.body)
   story.save((err,data)=>{
     if(err)
@@ -30,8 +85,10 @@ router.post('/',(req,res,next)=>{
 })
 
 router.get('/',(req,res,next)=>{
+  console.log("do this3")
   const promise = Story.find({})
   promise.then((data) => {
+    console.log("data",data)
     if(!data)
       next({message:'no data',code:5})
 		res.json(data);
