08b5affbd936111c13ffa9d1101836727f5226cf
Query Ballon <87583584+QueryBallon@users.noreply.github.com>
2023-09-30 12:18:06 +0800

    revamp user schema structure

--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,51 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const UserSchema = new Schema({
-    username:{
-        type:String,
-        unique:[true,"Please different username"]
+    // User Username
+    username: {
+        type: String,
+        required: true,
+        unique: [ true, "Username already exist, please login or use other username" ],
     },
-    name:String,
-    lastName:String,
-    public:{
-        type:Boolean,
+
+    // User Email
+    email: {
+        type: String,
+        required: true,
     },
+
+    // User Password
+    password: {
+        type: String,
+        required: true,
+    },
+
+    // User role
+    roles: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "role"
+        }
+    ],
+
+    // User Name
+    name: String,
+
+    // Last Name
+    lastName: String,
+
+    // Whether the user public
+    public: {
+        type: Boolean,
+        default: false
+    },
+
+    // User Profile Picture
     profilePhoto:{
-        type:String,
-        default:'default.jpg'
+        type: String,
+        default: 'default.jpg'
     },
-    createdDate:{
-        type:Date,
-        default:Date.now
-    }
 })
 
 module.exports = mongoose.model('user',UserSchema);
