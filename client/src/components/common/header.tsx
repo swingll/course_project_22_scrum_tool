d947930d7eaa077e5801593564e5a5f6b8bb7e25
Query Ballon <87583584+QueryBallon@users.noreply.github.com>
2023-10-03 23:04:06 +0800

    solved header link issue

--- client/src/components/common/header.tsx
+++ client/src/components/common/header.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import AddUser from '../forms/addUser';
 import { signout } from '../../states/user/service';
 import { useSignout } from '../../states/user/hooks';
@@ -23,9 +23,9 @@ function Header() {
       <div className="container containerDashboard">
         <div className="mainMenu">
           <ul>
-            <Link to="/story/1" activeClassName="active"><li><i className="fas fa-folder-open"></i><span className="mainMenuText">Projects</span></li></Link>
-            <Link to="/about" activeClassName="active"><li><i className="fas fa-thumbs-up" /><span className="mainMenuText">About</span></li></Link>
-            <Link to="/timeline" activeClassName="active"><li><i className="fas fa-thumbs-up"/><span className="mainMenuText">Timeline</span></li></Link>
+            <Link to="/story/1"><li><i className="fas fa-folder-open"></i><span className="mainMenuText">Projects</span></li></Link>
+            <Link to="/about"><li><i className="fas fa-thumbs-up" /><span className="mainMenuText">About</span></li></Link>
+            <Link to="/timeline"><li><i className="fas fa-thumbs-up"/><span className="mainMenuText">Timeline</span></li></Link>
             <a rel="noopener noreferrer" target="_blank" href="https://github.com/mreorhan/Scrum-Task-Management-with-ReactJS-Express-Server"><li><i className="fas fa-code-branch" /><span className="mainMenuText">Fork Me on Github</span></li></a>
           </ul>
         </div>
