b0aef3b53f3510b248eeeab3d26f977736031164
coolcheng222 <wo345@>
2023-10-08 22:19:21 +0800

    bug test

--- client/src/components/forms/addTask.tsx
+++ client/src/components/forms/addTask.tsx
@@ -4,6 +4,9 @@ import moment from 'moment'
 import { useCreateTask } from "../../states/task/hooks";
 import { useFetchStories } from "../../states/story/hooks";
 import {useFetchUsers, useUsers} from "../../states/user/hooks";
+// @ts-ignore
+import {NodeJS} from "timers";
+import {useRef} from "react";
 
 export function AddTask({ storyId, status, className,loading,setLoading }: any) {
   const [modal, setModal] = React.useState<boolean>(false);
@@ -11,17 +14,26 @@ export function AddTask({ storyId, status, className,loading,setLoading }: any) 
   const [content, setContent] = React.useState<string>('');
   const [contributors, setContributors] = React.useState<string>('');
   const [dueDate, setDueDate] = React.useState<string>('');
-  const [color, setColor] = React.useState<string>('');
+  const [color, setColor] = React.useState<string>('colorRed');
   const [err, setErr] = React.useState<string>('');
+  const [userLoading,setUserLoading] = React.useState<boolean>(false)
+  const [degree,setDegree]  = React.useState<number>(90)
+  const rotateInteval = useRef<NodeJS.Timeout>(undefined)
 
   const [createTask] = useCreateTask();
   const [fetchStories] = useFetchStories();
+  const [fetchUsers] = useFetchUsers()
   const users = useUsers() // TODO: fetch users
 
   React.useEffect(() => {
     moment.locale('hk');
     changeColumnTitle(0)
   }, [])
+  React.useEffect(()=>{
+    setUserLoading(false)
+    if(rotateInteval.current) clearInterval(rotateInteval.current)
+    rotateInteval.current = undefined
+  },[users])
 
   const changeColumnTitle = (number: number) => {
     switch (number) {
@@ -74,13 +86,27 @@ export function AddTask({ storyId, status, className,loading,setLoading }: any) 
     setModal(!modal)
     setErr('')
   }
+  const refeshUsers = () => {
+    if(rotateInteval){
+      clearInterval(rotateInteval.current)
+      rotateInteval.current = undefined
+    }
+    if(userLoading){
+      return;
+    }
+    setUserLoading(true)
+    fetchUsers()
+    rotateInteval.current = setInterval(()=>{
+      setDegree(degree=>degree + 5)
+    },50)
+  }
 
   let userContent;
-  if (!users || !users.length)
+  if (!users || !users.length || userLoading)
     userContent = <option value=''>Loading...</option>
   else {
     userContent = users.map((user: any, index: number) => (
-      <option key={index} value={user._id}>{user.name + ' ' + user.lastName}</option>
+      <option key={index} value={user._id}>{user.username}</option>
     ))
   }
   
@@ -104,13 +130,17 @@ export function AddTask({ storyId, status, className,loading,setLoading }: any) 
           <FormGroup>
             <Label for="contributors">Assign to:&nbsp;</Label>
             <Input type="select" name="contributors" id="contributors" onChange={(e) => setContributors(e.target.value)}>
-              {(users && users.length)?<option value="">Choose</option>:<></>}
+              {(users && users.length && !userLoading)?<option value="">Choose</option>:<></>}
               {userContent}
             </Input>
+            <Button className=" user-refresh" onClick={refeshUsers}>
+              <i className="fas fa-sync-alt" style={{rotate:`${degree}deg`}}></i>
+            </Button>
+
           </FormGroup>
           <FormGroup>
             <Label for="color">Task Color:&nbsp;</Label>
-            <Input type="select" name="color" id="color" onChange={(e) => setColor(e.target.value)}>
+            <Input type="select" defaultValue={'colorRed'} name="color" id="color" onChange={(e) => setColor(e.target.value)}>
               <option value="colorRed">Red</option>
               <option value="colorGreen">Green</option>
               <option value="colorGrey">Grey</option>
